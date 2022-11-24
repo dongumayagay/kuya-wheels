788634c545d26e4727797d3bf0b5fdaaf84a0c90
Kyle Basqueña <kyleandradebasquena@gmail.com>
2022-11-24 22:55:16 +0800

    tested if and variables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
 <script>
     import ExampleComponent from "$lib/components/ExampleComponent.svelte"
+    let show = false
+
+    function toggle(){
+        show = !show
+    }
 </script>
+
 <h1>Home page</h1>
-<ExampleComponent></ExampleComponent>
+<button on:click={toggle}>toggle</button>
+<h1>{show}</h1>
+{#if show == true}
+    <ExampleComponent></ExampleComponent>
+    {:else}
+    <h1>Esample</h1>
+{/if}
+
--- src/routes/book/+page.svelte
+++ src/routes/book/+page.svelte
@@ -1,14 +1,23 @@
 <script>
-	function submitHandler(event) {
-		const formData = new FormData(event.target);
-		const data = Object.fromEntries(formData);
-		console.log(data);
+	// function submitHandler(event) {
+	// 	const formData = new FormData(event.target);
+	// 	const data = Object.fromEntries(formData);
+	// 	console.log(data);
+	// }
+	let name = "kyle"
+	function submitHandler() {
+		console.log(name)
+
 	}
 </script>
 
 <h1>Book</h1>
 <form on:submit|preventDefault={submitHandler}>
-	<input type="text" name="fullname" placeholder="enter your full name" required />
-	<input type="text" name="email" placeholder="enter your email address" required />
+	<!-- <input type="text" name="name" placeholder="enter your full name" required />
+	<input type="text" name="email" placeholder="enter your email address" required /> -->
+	<input type="text" bind:value={name}>
+	{#if name.length<5}
+		<p>no no no</p>
+	{/if}
 	<button>Submit</button>
 </form>
