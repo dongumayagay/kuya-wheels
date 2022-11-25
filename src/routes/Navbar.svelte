a3b0c4f735fc9650e0903531dcd694d1045c0e18
Kyle Basqueña <kyleandradebasquena@gmail.com>
2022-11-26 00:47:18 +0800

    Renamed "contact" with "aboutus"

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,5 +1,31 @@
 <nav>
+
 	<a href="/">Home</a>
-	<a href="/contact">Contact</a>
+	<a href="/aboutus">About Us</a>
 	<a href="/book">Book</a>
 </nav>
+
+<style>
+	nav {
+		background-color: #cf332a;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
+		padding: 0.5em;
+	}
+	a {
+		display: flex;
+		align-items: flex-end;
+		font-size: 1.3em;
+		font-family: fantasy;
+		color: whitesmoke;
+		/* margin-right: 1.5em; */
+	}
+	a:link {
+		text-decoration: none;
+	}
+	a:hover{
+		text-decoration: underline;
+	}
+
+</style>
