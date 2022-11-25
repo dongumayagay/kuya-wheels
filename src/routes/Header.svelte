16f2fc0b259532684d06c4deafdb2d4c2666cbcc
Kyle Basqueña <kyleandradebasquena@gmail.com>
2022-11-26 00:46:07 +0800

    Set bg image and logo

---
+++ src/routes/Header.svelte
@@ -0,0 +1,18 @@
+<div>
+    <img src="/KWDSicon.png" alt="Kuya Wheels Driving School Icon" id="logo">
+</div>
+
+<style>
+    div {
+        background-image: url("/testbg.PNG");
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: 150px;
+        display: grid;
+        place-items: center;
+    }
+    img {
+        width: 250px;
+        /* height: 25%; */
+    }
+</style>
