f2a8b2e57a883fa2497faa8ba18110cb0127e571
Don Gumayagay <dngumayagay@gmail.com>
2022-11-26 10:58:39 +0800

    add page to test sending email

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { sendEmail } from '$lib/utils';
+
+	async function clickHandler() {
+		await sendEmail({
+			to: 'dngumayagay@gmail.com',
+			subject: 'Test 1',
+			html: '<h1>Hello World</ht>'
+		});
+	}
+</script>
+
+<button class="btn btn-info" on:click={clickHandler}> send email </button>
