74f7796e01d47e4e502f65594bafe67c9cb4832a
Kyle Basqueña <kyleandradebasquena@gmail.com>
2022-11-27 23:25:56 +0800

    minor changes

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,18 +1,36 @@
 <div>
     <img src="/KWDSicon.png" alt="Kuya Wheels Driving School Icon" id="logo">
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
 </div>
 
 <style>
+
     div {
-        background-image: url("/testbg.PNG");
+        /* background-image: url("/testbg.PNG");
         background-repeat: no-repeat;
-        background-size: cover;
+        background-size: cover; */
+        background-color: rgba(0,0, 0,0);
         height: 150px;
-        display: grid;
-        place-items: center;
+        display: flex;
     }
     img {
         width: 250px;
+        height: 95%;
         /* height: 25%; */
     }
+
+    h1{
+        font-size: 2em;
+        padding: 10px;
+    }
+/*     // experminted media query
+    @media screen and (max-width: 500px) {
+        img{
+            height: 50%;
+        }
+        h1{
+            font-size: 1em;
+            
+        }
+    } */
 </style>
