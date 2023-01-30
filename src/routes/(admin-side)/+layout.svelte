60444fbbbb6cff00b281ce5702bbfef0b1d61c1d
Kyle Basqueña <kyleandradebasquena@gmail.com>
2023-01-30 21:09:08 +0800

    restructure, and login page

---
+++ src/routes/(admin-side)/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { userStore } from '$lib/store.js'
+</script>
+
+{#if $userStore === undefined}
+    <div>
+        <h1>loading</h1>
+    </div>
+    {:else if $userStore !== undefined && $userStore !== null}
+    <slot></slot>
+{/if}
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { auth } from '$lib/firebase.js'
+    import { signInWithEmailAndPassword } from 'firebase/auth'
+    import { goto } from '$app/navigation'
+
+    let email, password
+
+    async function login() {
+        console.log(email, password)
+
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password)
+            goto("/admin")
+        } catch (error) {
+            console.log(error)
+            alert("Something went wrong logging you in")
+        }
+    }
+</script>
+
+<form on:submit|preventDefault={login}>
+    <input type="email" bind:value={email} required>
+    <input type="password" bind:value={password} required>
+    <button>login</button>
+</form>
