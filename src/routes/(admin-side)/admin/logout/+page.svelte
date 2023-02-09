2308a1f9f29522e343753d1e40fdfcf10b9834c3
Kyle Basqueña <kyleandradebasquena@gmail.com>
2023-02-09 22:35:19 +0800

    logout nav

---
+++ src/routes/(admin-side)/admin/logout/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { auth } from '$lib/firebase.js'
+    import { signOut } from 'firebase/auth'
+
+    async function logout() {
+
+        try {
+            await signOut(auth)
+        } catch (error) {
+            console.log(error)
+            alert("Something went wrong logging you out")
+        }
+    }
+</script>
+
+
+<button on:click={logout}>
+    logout
+</button>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&display=swap');
+
+    button{
+        font-family: 'Oswald';
+        font-weight: 400;
+        font-size: 30px;
+        
+        width: 150px;
+    }
+</style>
